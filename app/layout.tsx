5a2eb24007e35a4d1795ab7d2e47dd70feda60b9
SOHAM DEY <soham4707@gmail.com>
2025-07-15 13:52:13 +0530

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,12 @@ const RootLayout = async (props: Props) => {
 
     return (
         <html lang="en">
+            <head>
+                <link
+                  href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700&display=swap"
+                  rel="stylesheet"
+                />
+            </head>
             <body>
                 <Providers>
                     {children}
